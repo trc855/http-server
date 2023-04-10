5c3b956e878556a4c9d14af79172ae13ae4cb7d3
Thomas Cochrane <thomasrcochrane@icloud.com>
2023-04-10 17:40:42 +1000

    refactor for legibility

--- http_server.rb
+++ http_server.rb
@@ -1,4 +1,5 @@
 require 'socket'
+require_relative 'server_errors'
 
 socket = TCPServer.new(80)
 
@@ -10,18 +11,28 @@ loop do
   if verb == 'GET'
     if result = path.match(/^\/customers\/(.*?)$/)
       user_id = result[1]
+
       body = IO.read('example.html')
       body.gsub!('{{user_id}}', user_id)
+
       status = 200
+
       response = "HTTP/1.1 #{status}\r\nContent-Type: text/html\r\n\r\n#{body}"
+
       client.puts(response)
+    else
+      internal_server_error(client)
     end
   elsif verb == "HEAD"
     if result = path.match(/^\/customers\/(.*?)$/)
       status = 200
       response = "HTTP/1.1 #{status}\r\nContent-Type: text/html\r\n\r\n"
       client.puts(response)
+    else
+      internal_server_error(client)
     end
+  else
+    internal_server_error(client)
   end
 
   client.close
