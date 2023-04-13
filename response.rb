1d5109804ed7998b4bd23182ebeddd188328c29f
Thomas Cochrane <thomasrcochrane@icloud.com>
2023-04-13 15:47:41 +1000

    refactored responses to response.rb

---
+++ response.rb
@@ -0,0 +1,17 @@
+def get_response(result)
+  user_id = result[1]
+
+  body = IO.read('example.html')
+  body.gsub!('{{user_id}}', user_id)
+
+  response = "HTTP/1.1 200\r\n" +
+             "Content-Type: text/html\r\n" +
+             "\r\n" +
+             "#{body}"
+end
+
+def head_response
+  response = "HTTP/1.1 200\r\n" +
+             "Content-Type: text/html\r\n" +
+             "\r\n"
+end
